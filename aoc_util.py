5258d94d758433027e2bf06b0f349d0f9ef97c35
ryanpcooper <ryancooper@intricategroup.com>
2022-12-02 15:48:22 -0400

    day 2 part 2

--- aoc_util.py
+++ aoc_util.py
@@ -2,9 +2,13 @@
 
 import itertools
 import os
+import sys
 
 def getInput(scriptFile):
-	with open(os.path.splitext(os.path.basename(scriptFile))[0] + '.txt', 'r', encoding='latin-1') as f:
+	inputFileName = os.path.splitext(os.path.basename(scriptFile))[0] + '.txt'
+	if len(sys.argv) > 1:
+		inputFileName = sys.argv[1] + '.txt'
+	with open(inputFileName, 'r', encoding='latin-1') as f:
 		return [line.rstrip('\n') for line in f]
 
 def split(l, delimiter):
--- day2.py
+++ day2.py
@@ -4,13 +4,20 @@ from aoc_util import *
 
 lines = getInput(__file__)
 
-def roundScore(them, me):
+# part 1
+def roundScore1(them, me):
 	myChoice = ord(me)-87;
 	theirChoice = ord(them)-64;
 	resultPoints = [3, 6, 0][(myChoice-theirChoice)%3]
 	return resultPoints + myChoice
+scores = list(map(lambda l: roundScore1(*l.split(" ")), lines))
+print(sum(scores))
 
-scores = list(map(lambda l: roundScore(*l.split(" ")), lines))
-
-# part 1
+# part 2
+def roundScore2(them, desiredResult):
+	theirChoice = ord(them)-64;
+	myChoice = (theirChoice + ord(desiredResult)-89)%3 or 3
+	resultPoints = [3, 6, 0][(myChoice-theirChoice)%3]
+	return resultPoints + myChoice
+scores = list(map(lambda l: roundScore2(*l.split(" ")), lines))
 print(sum(scores))
