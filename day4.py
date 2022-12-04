93eba393834a768ef767977bf4fd55e9f11b57a9
ryanpcooper <ryancooper@intricategroup.com>
2022-12-04 11:58:08 -0400

    improved comment

--- day4.py
+++ day4.py
@@ -7,8 +7,8 @@ def full_overlap(p):
 	return (p[0] <= p[2] and p[1] >= p[3]) or (p[2] <= p[0] and p[3] >= p[1])
 	
 def partial_overlap(p):
-	elf1_sections = set(range(p[0], p[1]+1)) # ranges are exclusive to end point
-	elf2_sections = set(range(p[2], p[3]+1)) # ranges are exclusive to end point
+	elf1_sections = set(range(p[0], p[1]+1)) # ranges are exclusive of end point
+	elf2_sections = set(range(p[2], p[3]+1)) # ranges are exclusive of end point
 	overlap = len(elf1_sections.intersection(elf2_sections)) > 0
 
 # part 1
